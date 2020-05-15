0d5e7ee90a2a476fa3bbdfce18ac87753611e65f
Rajesh Borate <rajeshborate08@gmail.com>
2020-05-15 11:30:43 +0530

    Add files via upload

---
+++ charAndBoolean/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBoooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+        
+    }
+}
